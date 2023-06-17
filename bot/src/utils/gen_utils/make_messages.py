dcf9e8336c504834f51648646a68a4db99089206
soyelmismo <yo@resisto.rodeo>
2023-06-17 04:24:05 -0500

    yey

--- bot/src/utils/gen_utils/make_messages.py
+++ bot/src/utils/gen_utils/make_messages.py
@@ -1,12 +1,25 @@
 from bot.src.utils import config
 from bot.src.utils.constants import continue_key
 
-def process_dialog_message(dialog_message):
+def append_resources_messages(self, messages, dialog_messages):
+    documento_texts = []
+    url_texts = []
+    search_texts = []
+    for dialog_message in dialog_messages:
+        documento, url, search = process_resources_message(dialog_message)
+        documento_texts, url_texts, search_texts = append_resources_texts(documento, url, search, documento_texts, url_texts, search_texts, lang=self.lang)
+    if documento_texts or url_texts or search_texts:
+        messages.append({
+            "role": "system",
+            "content": f'{str(documento_texts) if documento_texts else ""}\n{str(url_texts) if url_texts else ""}\n{str(search_texts) if search_texts else ""}\n: {config.lang["metagen"]["contexto"][self.lang]}'
+        })
+    return messages
+def process_resources_message(dialog_message):
     documento = dialog_message.get("documento", "")
     url = dialog_message.get("url", "")
     search = dialog_message.get("search", "")
     return documento, url, search
-def append_message_texts(documento, url, search, documento_texts, url_texts, search_texts, lang):
+def append_resources_texts(documento, url, search, documento_texts, url_texts, search_texts, lang):
     if len(documento) > 1:
         documento_texts.append(f"'{config.lang['metagen']['documentos'][lang]}': {documento}")
     if len(url) > 1:
@@ -14,47 +27,46 @@ def append_message_texts(documento, url, search, documento_texts, url_texts, sea
     if len(search) > 1:
         search_texts.append(f"'{config.lang['metagen']['busquedaweb'][lang]}': {search}")
     return documento_texts, url_texts, search_texts
-def append_user_bot_messages(messages, dialog_message):
-    user = dialog_message.get("user", "")
-    bot = dialog_message.get("bot", "")
-    if len(user) >= 2:
-        messages.append({"role": "user", "content": user})
-    if len(bot) >= 2:
-        messages.append({"role": "assistant", "content": bot})
+
+def append_user_bot_messages(messages, dialog_messages):
+    for dialog_message in dialog_messages:
+        user = dialog_message.get("user", "")
+        bot = dialog_message.get("bot", "")
+        if len(user) >= 2:
+            messages.append({"role": "user", "content": user})
+        if len(bot) >= 2:
+            messages.append({"role": "assistant", "content": bot})
+    return messages
+
+def append_chat_mode(self, chat_mode, messages):
+    if chat_mode == "imagen":
+        messages.append({"role": "system", "content": f'{config.chat_mode["info"][chat_mode]["prompt_start"]}'})
+    elif chat_mode != "nada":
+        language = config.lang["info"]["name"][self.lang]
+        especificacionlang = config.lang["metagen"]["especificacionlang"].format(language=language)
+        prompter = config.chat_mode["info"][chat_mode]["prompt_start"].format(language=language)
+        injectprompt = """{especificarlang}\n\n{elprompt}\n\n{especificarlang}\n\n"""
+        messages.append({"role": "system", "content": injectprompt.format(especificarlang=especificacionlang, elprompt=prompter)})
+    return messages
+
+def continue_or_append_latest_message(_message, messages):
+    if _message == continue_key:
+        _message = ""
+        last_assistant_message_index = next((index for index, message in reversed(list(enumerate(messages))) if message["role"] == "assistant"), -1)
+        if last_assistant_message_index != -1:
+            content = messages[last_assistant_message_index]["content"]
+            if content.endswith("."): messages[last_assistant_message_index]["content"] = content[:-2]
+    else:
+        messages.append({"role": "user", "content": _message})
     return messages
 
 async def handle(self, _message="", dialog_messages=[], chat_mode="nada"):
     try:
         messages = []
-        documento_texts = []
-        url_texts = []
-        search_texts = []
-        for dialog_message in dialog_messages:
-            documento, url, search = process_dialog_message(dialog_message)
-            documento_texts, url_texts, search_texts = append_message_texts(documento, url, search, documento_texts, url_texts, search_texts, lang=self.lang)
-        if documento_texts or url_texts or search_texts:
-            messages.append({
-                "role": "system",
-                "content": f'{str(documento_texts) if documento_texts else ""}\n{str(url_texts) if url_texts else ""}\n{str(search_texts) if search_texts else ""}\n: {config.lang["metagen"]["contexto"][self.lang]}'
-            })
-        for dialog_message in dialog_messages:
-            messages = append_user_bot_messages(messages, dialog_message)
-        if chat_mode == "imagen":
-            messages.append({"role": "system", "content": f'{config.chat_mode["info"][chat_mode]["prompt_start"]}'})
-        elif chat_mode != "nada":
-            language = config.lang["info"]["name"][self.lang]
-            especificacionlang = config.lang["metagen"]["especificacionlang"].format(language=language)
-            prompter = config.chat_mode["info"][chat_mode]["prompt_start"].format(language=language)
-            injectprompt = """{especificarlang}\n\n{elprompt}\n\n{especificarlang}\n\n"""
-            messages.append({"role": "system", "content": injectprompt.format(especificarlang=especificacionlang, elprompt=prompter)})
-        if _message == continue_key:
-            _message = ""
-            last_assistant_message_index = next((index for index, message in reversed(list(enumerate(messages))) if message["role"] == "assistant"), -1)
-            if last_assistant_message_index != -1:
-                content = messages[last_assistant_message_index]["content"]
-                if content.endswith("."): messages[last_assistant_message_index]["content"] = content[:-2]
-        else:
-            messages.append({"role": "user", "content": _message})
+        messages = append_resources_messages(self, messages, dialog_messages)
+        messages = append_user_bot_messages(messages, dialog_messages)
+        messages = append_chat_mode(self, chat_mode, messages)
+        messages = continue_or_append_latest_message(_message, messages)
         messages = [message for message in messages if message["content"]]
         return messages
     except Exception as e:
--- bot/src/utils/gen_utils/make_prompt.py
+++ bot/src/utils/gen_utils/make_prompt.py
@@ -1,9 +1,25 @@
 from bot.src.utils import config
 from bot.src.utils.constants import continue_key
 
-def get_texts(dialog_messages, key):
-    texts = [dialog_message.get(key, "").strip() for dialog_message in dialog_messages if len(dialog_message.get(key, "")) >= 5]
-    return "\n".join(texts) if texts else ""
+def get_resources_texts(dialog_messages, key):
+    for dialog_message in dialog_messages:
+        if len(dialog_message.get(key, "")) >= 5:
+            texts = dialog_message.get(key, "").strip()
+    if texts:
+        return "\n".join(texts)
+    return ""
+def append_resources_texts(self, documento_texts, url_texts, search_texts, prompt):
+    resources = ""
+    if documento_texts:
+        resources += f'{config.lang["metagen"]["documentos"][self.lang]}: {documento_texts}\n\n'
+    if url_texts:
+        resources += f'{config.lang["metagen"]["urls"][self.lang]}: {url_texts}\n\n'
+    if search_texts:
+        resources += f'{config.lang["metagen"]["busquedaweb"][self.lang]}: {search_texts}\n\n'
+    if len(resources) > 4:
+        prompt += resources
+        prompt += f'^{config.lang["metagen"]["mensaje"][self.lang]}: {prompt}][{config.lang["metagen"]["contexto"][self.lang]}^'
+    return prompt
 
 def get_prompt_lines(dialog_messages, chat_mode, lang):
     prompt_lines = []
@@ -21,32 +37,39 @@ def get_injectprompt(language, prompter):
     injectprompt = """{especificarlang}\n\n{elprompt}\n\n{especificarlang}\n\n"""
     return injectprompt.format(especificarlang=especificacionlang, elprompt=prompter)
     
+def append_chat_mode(self, chat_mode, prompt):
+    if chat_mode == "imagen":
+        prompt += f'{config.chat_mode["info"][chat_mode]["prompt_start"]}'
+    elif chat_mode != "nada":
+        language = config.lang["info"]["name"][self.lang]
+        prompter = config.chat_mode["info"][chat_mode]["prompt_start"].format(language=language)
+        prompt += get_injectprompt(language, prompter)
+    return prompt
+
+def continue_or_append_latest_message(self, _message, prompt, chat_mode):
+    if _message == continue_key:
+        _message = ""
+        if prompt.endswith(".") or prompt.endswith("?"):
+            prompt = prompt[:-3]
+    else:
+        prompt += f'{config.lang["metagen"]["usuario"][self.lang]}: {_message}'
+        if chat_mode != "nada": prompt += f'{config.chat_mode["info"][chat_mode]["name"][self.lang]}:'
+    return prompt
+
 async def handle(self, _message="", dialog_messages=[], chat_mode="nada"):
     try:
         prompt = ""
         documento_texts = ""
         url_texts = ""
         search_texts = ""
-        documento_texts = get_texts(dialog_messages, "documento")
-        url_texts = get_texts(dialog_messages, "url")
-        search_texts = get_texts(dialog_messages, "search")
-        if documento_texts or url_texts or search_texts:
-            prompt += f'{config.lang["metagen"]["documentos"][self.lang]}: {documento_texts}\n\n{config.lang["metagen"]["urls"][self.lang]}: {url_texts}\n\n{config.lang["metagen"]["busquedaweb"][self.lang]}: {search_texts}\n\n{config.lang["metagen"]["mensaje"][self.lang]}: [{prompt}][{config.lang["metagen"]["contexto"][self.lang]}]'
-        prompt += f'{config.lang["metagen"]["log"][self.lang]}:\n'
+        documento_texts = get_resources_texts(dialog_messages, "documento")
+        url_texts = get_resources_texts(dialog_messages, "url")
+        search_texts = get_resources_texts(dialog_messages, "search")
+        prompt = append_resources_texts(self, documento_texts, url_texts, search_texts, prompt)
+        prompt += f'{config.lang["metagen"]["log"][self.lang]}:\n\n'
         prompt += get_prompt_lines(dialog_messages, chat_mode, lang=self.lang)
-        if chat_mode == "imagen":
-            prompt += f'{config.chat_mode["info"][chat_mode]["prompt_start"]}'
-        elif chat_mode != "nada":
-            language = config.lang["info"]["name"][self.lang]
-            prompter = config.chat_mode["info"][chat_mode]["prompt_start"].format(language=language)
-            prompt += get_injectprompt(language, prompter)
-        if _message == continue_key:
-            _message = ""
-            if prompt.endswith(".") or prompt.endswith("?"):
-                prompt = prompt[:-3]
-        else:
-            prompt += f'{config.lang["metagen"]["usuario"][self.lang]}: {_message}'
-            if chat_mode != "nada": prompt += f'{config.chat_mode["info"][chat_mode]["name"][self.lang]}:'
+        prompt = append_chat_mode(self, chat_mode, prompt)
+        prompt = continue_or_append_latest_message(self, _message, prompt, chat_mode)
         return prompt
     except Exception as e:
         e = f'_generate_prompt: {e}'
